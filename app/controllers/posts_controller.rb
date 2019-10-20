485069bbf37c6374c153a3bea7a0aaeea8d774f4
19980410 <kkaoru1998@gmail.com>
2019-10-21 01:50:14 +0900

    afs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
     posts = Post.all
     post.name = dm
     posts.each do |f|
-      a = f.name¥n 
+      a += f.name 
     end
    
     if user_signed_in?
