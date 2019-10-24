6b5ffb287205727d9f812c964a357f17d4a06afc
19980410 <kkaoru1998@gmail.com>
2019-10-25 00:00:47 +0900

    afs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -160,26 +160,6 @@ class PostsController < ApplicationController
             # }
           ]
         }
-      },
-      {
-        "type": "template",
-        "altText": "this is template",
-        "template":{
-          "type": "confirm",
-          "text": "まぜそばは好きですか？",
-          "actions": [
-            {
-              "type": "message",
-              "label": "カメラを起動する",
-              "text": "カメラを起動する"
-            },
-            {
-              "type": "message",
-              "label": "アルバム",
-              "text": "アルバムを開く"
-            }
-          ]
-        }
       }
     end
 
